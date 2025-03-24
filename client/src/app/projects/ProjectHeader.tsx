53739e675f754f52beaa3a4249d0af4c969f5235
Ryan Hui <ryanhui30@gmail.com>
2025-03-24 16:37:56 -0700

    Add create project modal and button.

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
-import { Clock, Filter, Grid3x3, List, Share2, Table } from 'lucide-react';
+import { Clock, Filter, Grid3x3, List, PlusSquare, Share2, Table } from 'lucide-react';
 import React, { useState } from 'react'
+import ModalNewProject from "./ModalNewProject"
 
 type Props = {
   activeTab: string;
@@ -11,9 +12,21 @@ const ProjectHeader = ({activeTab, setActiveTab}: Props) => {
   const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
 
   return <div className="px-4 xl:px-6">
-    {/* Modal New Project */}
+     <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
     <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-      <Header name="Product Design Development" />
+      <Header name="Product Design Development"
+        buttonComponent={
+          <button
+            className="flex items-center rounded-md bg-blue-primary px-3 py-2"
+            onClick={()=> setIsModalNewProjectOpen(true)}
+          >
+            <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+          </button>
+        }
+      />
     </div>
 
     {/* Tabs */}
