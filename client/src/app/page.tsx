48314946bb64c2f1f20f730bb11993c149dfc45f
Ryan Hui <ryanhui30@gmail.com>
2025-03-26 13:29:42 -0700

    Add home page with project management dashboard charts and task list.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import HomePage from "./home/page";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        hi
-    </main>
-  );
+  return <HomePage />;
 }
