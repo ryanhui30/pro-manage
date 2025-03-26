49f39479b397e10dacddd5c57d8dbf680d026239
Ryan Hui <ryanhui30@gmail.com>
2025-03-26 13:15:16 -0700

    Add team page front and backend.

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,7 @@ import projectRoutes from "./routes/projectRoutes"
 import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import userRoutes from "./routes/userRoutes";
+import teamRoutes from "./routes/teamRoutes";
 
 /* Configurations */
 dotenv.config();
@@ -31,6 +32,7 @@ app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
 
 /* Server */
 const port = Number(process.env.PORT) || 3000;
