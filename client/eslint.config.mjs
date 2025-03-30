c2a1c067e0ef22001d5bbefe89700dcff31232bf
Ryan Hui <ryanhui30@gmail.com>
2025-03-29 18:40:56 -0700

    Fix build errors for [id] page.tsx and eslint.config.mjs.

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,10 +13,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
     rules: {
+      '@next/next/no-html-link-for-pages': 'error',
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
@@ -25,7 +30,8 @@ const eslintConfig = [
         }
       ]
     }
-  }
+  },
+  ...compat.extends("next/core-web-vitals")
 ];
 
 export default eslintConfig;
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -7,16 +7,15 @@ import List from "../ListView";
 import Timeline from "../TimelineView";
 import Table from "../TableView";
 import ModalNewTask from '@/components/ModalNewTask';
+import { useParams } from 'next/navigation';
 
-type Props = {
-    params: { id: string };
-    searchParams?: Record<string, string | string[] | undefined>;
-};
-
-const Project = ({ params }: Props) => {
-    const { id } = params;
+const Project = () => {
+    const params = useParams();
+    const id = Array.isArray(params.id) ? params.id[0] : params.id;
     const [activeTab, setActiveTab] = useState("Board");
     const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+    if (!id) return <div>Project not found</div>;
 
     return (
         <div>
@@ -26,7 +25,7 @@ const Project = ({ params }: Props) => {
                 id={id}
             />
             <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-            {activeTab == "Board" && (
+            {activeTab === "Board" && (
                 <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
             )}
             {activeTab === "List" && (
